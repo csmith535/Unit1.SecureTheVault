801ca6b2ac39c16d32635954a5818acd620bdff4
Cameron Smith <cameron.smith3030@gmail.com>
2025-07-23 21:53:27 -0400

    updated index.js to meet req and linked to index.html

---
+++ index.js
@@ -0,0 +1,17 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = 100 / 2 - 10;
+
+let code3 = (10 % 3) * 39;
+
+let message = "The vault has been secured. The combination is:";
+
+let codeA = code1 - code2 - code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
